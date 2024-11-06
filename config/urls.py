884f3ea8c992f4a7085e0cd3baf69d49622c2186
setharasipalli mani <manisetharasipalli@gmail.com>
2024-11-06 15:27:58 +0530

    modified the url

--- config/urls.py
+++ config/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("tweet/", include("tweets.urls"))
+    path("api/", include("tweets.urls"))
     
 ]
