64dac3ab2cf333caa7eec3a734ef97e2eb9ea462
setharasipalli mani <manisetharasipalli@gmail.com>
2024-11-06 12:28:19 +0530

    upadted and add extra models

--- tweets/models.py
+++ tweets/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Tweets(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    deleted_at = models.DateTimeField(null=True, blank=True)
+
+
+    def __str__(self):
+        return f'Tweet By {self.user.username} at {self.created_at}'
+
+    
+
+
+
+class Comments(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    tweet = models.ForeignKey(Tweets, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+    def __str__(self):
+        return f'Commented on {self.tweet.content} at {self.created_at}'    
