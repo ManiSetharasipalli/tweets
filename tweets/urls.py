dfaedbf3b9fd80bd18fb5f4865cd9efc1faaa301
setharasipalli mani <manisetharasipalli@gmail.com>
2024-11-07 19:28:53 +0530

    authentication is done

--- tweets/urls.py
+++ tweets/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path
-from .views import UserRegistrationAPI
+from .views import UserRegistrationAPI, Users
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView
+
+)
 
 urlpatterns = [
-    path('register',UserRegistrationAPI.as_view(), name="user-register")
+    path('register',UserRegistrationAPI.as_view(), name="user-register"),
+    path('token', TokenObtainPairView.as_view(), name="token_obtain_pair_view"),
+    path('token/refresh', TokenRefreshView.as_view(), name='token_refresh_view'),
+    path('users', Users.as_view(), name='get_all_users')
 ]
--- tweets/views.py
+++ tweets/views.py
@@ -2,6 +2,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from .serializers import UserRegistrationSerializer
+from rest_framework.permissions import IsAuthenticated
 
 
 class UserRegistrationAPI(APIView):
@@ -11,3 +12,9 @@ class UserRegistrationAPI(APIView):
             serializer.save()
             return Response({"message": "User registered successfully"}, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class Users(APIView):
+    permission_classes = [IsAuthenticated]
+    def get(self, request):
+        return Response({'message':'This is procted API'})
